6778e1c5e7f7b0fa1ab58f32f62fbddd768cbd5c
Val <valmpitts@gmail.com>
2026-03-07 16:29:22 -0500

    Initial Commit; Provided basic structure for webpage outline.

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+      <header>
+        <div className="logo">Val Bytes</div>
+        <div className="nav">Nav</div>
+      </header>
+      <main>
+        <section className="hero">Hero</section>
+        <section className="services">Services</section>
+        <section className="contact">Contact</section>
+        <section className="about">About</section>
+      </main>
+      <footer>
+        <div className="footer-content">Footer</div>
+      </footer>
+
+    </div >
+
+  )
+}
+
+export default App
