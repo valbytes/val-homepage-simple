373d8464f5ab5fae7ee89fb0f19efe0364d10ac2
Val <valmpitts@gmail.com>
2026-03-25 20:56:28 -0400

    Edits to footer

--- src/assets/pageSections/Footer/Footer.tsx
+++ src/assets/pageSections/Footer/Footer.tsx
@@ -1,11 +1,12 @@
+import './Footer.css';
 
 function Footer() {
     return (
         <section>
             <div className="footer-container">
                 <div className="footer-content">
-                    <h2>Valerie Pitts</h2>
-                    <p>Full Stack Developer</p>
+                    <h2>Val.bytes</h2>
+                    <p>Web Developer</p>
                     <p>Copyright &copy; 2026</p>
                 </div>
             </div>
